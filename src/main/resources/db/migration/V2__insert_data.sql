1e6082641943356ca124866132671e179a0c4ae4
Jacek Lato <jacek_lato@o2.pl>
2023-10-01 11:11:19 +0200

    refactor

--- src/main/resources/db/migration/V2__insert_data.sql
+++ src/main/resources/db/migration/V2__insert_data.sql
@@ -1,14 +1,14 @@
 INSERT INTO POLITICIAN_ENTITY (ID,  NAME, SURNAME, BIO, POLITICAL_PARTY, PROFILE_PHOTO_PATH)
-VALUES (1, 'John', 'Doe', 'A dedicated public servant', 'KONFEDERACJA', '/profiles/1_john_doe.jpg');
+VALUES (1, 'John', 'Doe', 'A dedicated public servant', 'Democrats', '/profiles/1_john_doe.jpg');
 
 INSERT INTO POLITICIAN_ENTITY (ID, NAME, SURNAME, BIO, POLITICAL_PARTY, PROFILE_PHOTO_PATH)
-VALUES (2, 'Alice', 'Smith', 'Passionate about social issues', 'KOALICJA_OBYWATELSKA', '/profiles/2_alice_smith.jpg');
+VALUES (2, 'Alice', 'Smith', 'Passionate about social issues', 'Democrats', '/profiles/2_alice_smith.jpg');
 
 INSERT INTO POLITICIAN_ENTITY (ID, NAME, SURNAME, BIO, POLITICAL_PARTY, PROFILE_PHOTO_PATH)
-VALUES (3, 'Bob', 'Johnson', 'Experienced legislator', 'KONFEDERACJA', '/profiles/3_bob_johnson.jpg');
+VALUES (3, 'Bob', 'Johnson', 'Experienced legislator', 'Republican ', '/profiles/3_bob_johnson.jpg');
 
 INSERT INTO POLITICIAN_ENTITY (ID, NAME, SURNAME, BIO, POLITICAL_PARTY, PROFILE_PHOTO_PATH)
-VALUES (4, 'Emily', 'Brown', 'Champion of environmental causes', 'PIS', '/profiles/4_emily_brown.jpg');
+VALUES (4, 'Emily', 'Brown', 'Champion of environmental causes', 'Libertarian ', '/profiles/4_emily_brown.jpg');
 
 INSERT INTO POLITICIAN_ENTITY (ID, NAME, SURNAME, BIO, POLITICAL_PARTY, PROFILE_PHOTO_PATH)
-VALUES (5, 'Michael', 'Lee', 'Advocate for education reform', 'PIS', '/profiles/5_michael_lee.jpg');
+VALUES (5, 'Michael', 'Lee', 'Advocate for education reform', 'Libertarian ', '/profiles/5_michael_lee.jpg');
