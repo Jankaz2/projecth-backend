29b9e04d2bc0603263443de2ea7408c5cdf274be
Jacek Lato <jacek_lato@o2.pl>
2023-10-01 11:11:54 +0200

    refactor

--- src/main/java/com/projecth/projecthbackend/politician/follow/FollowController.java
+++ src/main/java/com/projecth/projecthbackend/politician/follow/FollowController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/followed/")
+@RequestMapping("/api/v1/followed")
 @RequiredArgsConstructor
 public class FollowController {
 
